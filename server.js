3154af9ed1dd0ee352f1fc35a7b0f92464d0a481
kurabachew Mengistu <kmengistu1216@gmail.com>
2026-06-05 10:58:28 -0500

    fix PORT variable

--- server.js
+++ server.js
@@ -7,16 +7,19 @@ import db from "./config/db.js";
 import cors from "cors";
 
 const app = express();
-const port = process.env.PORT || 3001;
+
+const PORT = process.env.PORT || 3001;
+
 app.use(cors());
 app.use(express.json());
+
 app.use("/api", router);
+
 app.get("/", (req, res) => {
   res.json({
-    msg: `Mesi Catering Backend Running `,
+    msg: "Mesi Catering Backend Running",
   });
 });
-
 
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
