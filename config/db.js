9ea309b528c3d345551864bc992129bfc2b476f9
kurabachew Mengistu <kmengistu1216@gmail.com>
2026-06-08 15:58:11 -0500

    fix production database env

--- config/db.js
+++ config/db.js
@@ -1,17 +1,19 @@
-import mysql from 'mysql2/promise'
-import dotenv from 'dotenv'
+import mysql from "mysql2/promise";
+import dotenv from "dotenv";
+
 dotenv.config();
 
-const db =  mysql.createPool({
-     host: process.env.DB_HOST,
-     user: process.env.DB_USER,
-     password: process.env.DB_PASSWORD,
-     database: process.env.DB_NAME,
-     port:3306
+const db = mysql.createPool({
+  host: process.env.DB_HOST,
+  port: process.env.DB_PORT,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_NAME, // ✅ FIXED
+  
+});
+console.log("DB_HOST:", process.env.DB_HOST);
+console.log("DB_PORT:", process.env.DB_PORT);
+console.log("DB_USER:", process.env.DB_USER);
+console.log("DB_NAME:", process.env.DB_NAME);
 
-
-    
-})
-
-
-export default db
+export default db;
